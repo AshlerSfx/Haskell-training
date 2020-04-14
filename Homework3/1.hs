a68ac526eded5a16c429c22e4bf4487c6a9cf8d8
AshlerSfx <43908347+AshlerSfx@users.noreply.github.com>
2020-04-14 21:52:58 +0300

    Update 1.hs

--- Homework3/1.hs
+++ Homework3/1.hs
@@ -1,6 +1,6 @@
---func x l = map (\y -> x * y) l применяем бета-редукцию по l
+--func x l = map (\y -> x * y) l применяем eta - редукцию по l
 --func x = map (\y -> y*x) заменяем лямбда выражение
 --func x = map (*x) применяем композицию f.g
---func x = map.(*) $x бета-редукция x
+--func x = map.(*) $x eta-редукция x
 
 func = map.(*)
