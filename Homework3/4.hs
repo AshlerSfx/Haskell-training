dfa484189bb2339322d570e6d4a1f18f5d03989a
AshlerSfx <43908347+AshlerSfx@users.noreply.github.com>
2020-04-15 17:35:46 +0300

    Update 4.hs
    Правильная скобочная последовательность не подразумевает "]["

--- Homework3/4.hs
+++ Homework3/4.hs
@@ -6,9 +6,9 @@ checkString string = helper string 0 0 0
 helper [] br1  br2 br3 = if((br1 == 0) && (br2 == 0) && (br3 == 0)) then True else False
 
 helper (x:xs) br1 br2 br3                |x == '(' = helper xs (br1 + 1) br2 br3
-                                         |x == ')' = helper xs (br1 - 1) br2 br3
+                                         |x == ')' = if br1 == 0 then False else helper xs (br1 - 1) br2 br3
                                          |x == '[' = helper xs br1 (br2 + 1) br3
-                                         |x == ']' = helper xs br1 (br2 - 1) br3
+                                         |x == ']' = if br2 == 0 then False else helper xs br1 (br2 - 1) br3
                                          |x == '{' = helper xs br1 br2 (br3 + 1)
-                                         |x == '}' = helper xs br1 br2 (br3 - 1)
+                                         |x == '}' = if br3 == 0 then False else helper xs br1 br2 (br3 - 1)
                                          |otherwise = helper xs br1 br2 br3 
