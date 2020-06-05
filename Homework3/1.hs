4a5ed38ef82ed74a19576655dae632cfb469c62c
AshlerSfx <43908347+AshlerSfx@users.noreply.github.com>
2020-06-05 13:27:21 +0300

    Delete 1.hs

--- Homework3/1.hs
+++
@@ -1,6 +0,0 @@
---func x l = map (\y -> x * y) l применяем eta - редукцию по l
---func x = map (\y -> y*x) заменяем лямбда выражение
---func x = map (*x) применяем композицию f.g
---func x = map.(*) $x eta-редукция x
-
-func = map.(*)
