f94f01b1d4bfaeb1adbfb3956a8cfdbb0f3c62db
AshlerSfx <43908347+AshlerSfx@users.noreply.github.com>
2020-06-05 13:28:26 +0300

    Delete 2.hs

--- Homework2/2.hs
+++
@@ -1,7 +0,0 @@
-twoPowersList :: Int->[Int]
-helper :: [Int]->Int->Int->[Int]
-
-twoPowersList n = helper [] 1 n
-
-helper list cur_num 0 = list ++ [cur_num]
-helper list cur_num n = helper (list ++ [cur_num]) (2*cur_num) $n-1
