150a381cb01b359d6d2ec81736292cc1bbf72a14
AshlerSfx <43908347+AshlerSfx@users.noreply.github.com>
2020-06-06 04:55:24 +0300

    Update 4.hs

--- Homework3/4.hs
+++ Homework3/4.hs
@@ -1,14 +1,16 @@
 checkString :: [Char]->Bool
-helper :: [Char]->Int->Int->Int->Bool
+helper :: [Char]->[Char]->Bool
 
-checkString string = helper string 0 0 0
 
-helper [] br1  br2 br3 = if((br1 == 0) && (br2 == 0) && (br3 == 0)) then True else False
+checkString string = helper string [] 
 
-helper (x:xs) br1 br2 br3                |x == '(' = helper xs (br1 + 1) br2 br3
-                                         |x == ')' = if br1 == 0 then False else helper xs (br1 - 1) br2 br3
-                                         |x == '[' = helper xs br1 (br2 + 1) br3
-                                         |x == ']' = if br2 == 0 then False else helper xs br1 (br2 - 1) br3
-                                         |x == '{' = helper xs br1 br2 (br3 + 1)
-                                         |x == '}' = if br3 == 0 then False else helper xs br1 br2 (br3 - 1)
-                                         |otherwise = helper xs br1 br2 br3 
+helper []  buf = if buf == [] then True else False
+
+helper (x:xs) buf             |x == '(' = helper xs (x:buf)
+                              |x == ')' = if (head buf /= '(') then False else helper xs $tail buf
+                              |x == '[' = helper xs (x:buf)
+                              |x == ']' = if (head buf /= '[') then False else helper xs $tail buf
+                              |x == '{' = helper xs (x:buf)
+                              |x == '}' = if (head buf /= '{') then False else helper xs $tail buf
+                              |otherwise = helper xs buf 
+
